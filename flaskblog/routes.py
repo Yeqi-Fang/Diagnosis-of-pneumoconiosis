be33b7f5f1a96fb96482a48b7704fa1bc8e57ac7
FYQ <fangyeqi202106@163.com>
2023-02-17 13:03:59 +0800

    23-2-17-3

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -103,10 +103,8 @@ def upload_file(file_name, bucket, object_name=None):
     if object_name is None:
         object_name = os.path.basename(file_name)
 
-    # Upload the file
-    s3_client = boto3.client('s3')
     try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
+        response = s3.upload_file(file_name, bucket, object_name)
     except ClientError as e:
         print(e)
         return False
