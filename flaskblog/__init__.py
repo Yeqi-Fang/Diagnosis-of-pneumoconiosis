c540b56d2e8203e768316fcbbb017c9b704fbf37
FYQ <fangyeqi202106@163.com>
2023-02-17 12:07:46 +0800

    23-2-17-1

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # sqlite:///site.db
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp-mail.outlook.com'  #
