b602f6046aaeaf94670df744902c2850228c4697
fagnyeqi <103356538+Yeqi-Fang@users.noreply.github.com>
2023-03-23 20:42:23 +0800

    3-22-2

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,7 +3,7 @@ import os
 import boto3
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+# from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -32,7 +32,7 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 mail = Mail(app)
-migrate = Migrate(app, db)
+# migrate = Migrate(app, db)
 # s3 = FlaskS3(app)
 
 login_manager.login_view = 'login'
